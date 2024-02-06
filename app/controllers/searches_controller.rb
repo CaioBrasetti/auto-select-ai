6b844d4d4e7d02dc47bfc38a9d5b6055f34ccd97
CaioBrasetti <caiobrasetti@hotmail.com>
2024-02-06 18:22:50 -0300

    Feat: Implementação da API do chatGPT e logica da controller e view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,11 +7,21 @@ class SearchesController < ApplicationController
   def create_prompt
     @search = Search.new(search_params)
     if @search.save
-      redirect_to root_path, notice: 'Criado com sucesso.'
+      redirect_to show_path(@search.id), notice: 'Criado com sucesso.'
     else
       @search = Search.all
       render :index
     end
+  end
+
+  def show
+    client = OpenAI::Client.new
+    chaptgpt_response = client.chat(parameters:
+      {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "me fale sobre a marca #{@search} 2 linhas no maximo"}]
+      })
+    @content = chaptgpt_response["choices"][0]["message"]["content"]
   end
 
   private
