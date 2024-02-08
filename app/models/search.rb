ffd46140dfa9030eb6d88e9e7123e0c5bc502f68
anthony vieira <thony16@gmail.com>
2024-02-08 17:11:26 -0300

    feat: new columns, optionals WIP

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,11 +1,11 @@
 class Search < ApplicationRecord
-  def content(text)
+  def content(optionals5)
     Rails.cache.fetch("#{cache_key_with_version}/content") do # verificar essa linha
     client = OpenAI::Client.new
     chaptgpt_response = client.chat(parameters: {
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Me fale um carro de cada marca, sendo elas: #{text}."}]
-    })
+      messages: [{ role: "user", content: "Me mostre um carro que seja #{optionals5}, e que possua cambio #{optionals5}." }]
+      })
     return chaptgpt_response["choices"][0]["message"]["content"]
     end
   end
--- db/migrate/20240130153812_create_searches.rb
+++ db/migrate/20240130153812_create_searches.rb
@@ -1,8 +1,11 @@
 class CreateSearches < ActiveRecord::Migration[7.1]
   def change
     create_table :searches do |t|
-      t.string :text
-
+      t.string :body1
+      t.string :transmission2
+      t.string :engine3
+      t.string :traction4
+      t.string :optionals5
       t.timestamps
     end
   end
