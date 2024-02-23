6670acee9ae143a2439fd14be67d8606a79abc3d
CaioBrasetti <caiobrasetti@hotmail.com>
2024-02-23 19:05:01 -0300

    Adicioanando os outros campos do prompt, arrumando a view e a show

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,20 @@ class SearchesController < ApplicationController
   end
 
   def create_prompt
-    converted_variable_in_string = search_params[:optionals5].join(', ')
-    @search = Search.new(optionals5: converted_variable_in_string)
+    body1_value = search_params[:body1].presence || ""
+    transmission2_value = search_params[:transmission2].presence || ""
+    engine3_value = search_params[:engine3].presence || ""
+    traction4_value = search_params[:traction4].presence || ""
+    optionals5_converted_variable_in_string = search_params[:optionals5].present? ? search_params[:optionals5].join(', ') : ""
+
+    @search = Search.new(
+      body1: body1_value,
+      transmission2: transmission2_value,
+      engine3: engine3_value,
+      traction4: traction4_value,
+      optionals5: optionals5_converted_variable_in_string
+    )
+
     if @search.save
       redirect_to show_path(@search.id), notice: 'Criado com sucesso.'
     else
@@ -17,7 +29,13 @@ class SearchesController < ApplicationController
 
   def show
     search = Search.find(params[:id])
-    @content = Search.new.content(search.optionals5)
+    @content = Search.new.content(
+      search.body1,
+      search.transmission2,
+      search.engine3,
+      search.traction4,
+      search.optionals5
+    )
   end
 
   private
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,10 +1,15 @@
 class Search < ApplicationRecord
-  def content(optionals5)
+  def content(body1, transmission2, engine3, traction4, optionals5)
     Rails.cache.fetch("#{cache_key_with_version}/content") do # verificar essa linha
     client = OpenAI::Client.new
     chaptgpt_response = client.chat(parameters: {
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Me mostre um carro que seja #{optionals5}, e que possua cambio #{optionals5}." }]
+      messages: [{ role: "user", content: "Me de 5 opções de carros que se enquadram o mais proximo possivel dessas caracteristicas:
+        carroceria #{body1},
+        trasmissao #{transmission2}
+        motorização #{engine3}
+        Tração #{traction4}
+        com os opcionais#{optionals5}." }]
       })
     return chaptgpt_response["choices"][0]["message"]["content"]
     end
